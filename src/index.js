af297dfc45756dcfba22a0b4c026120331834ce3
firdausthedev <firdausthedev@gmail.com>
2021-11-30 18:01:56 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <p class='title'>In development</p>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
