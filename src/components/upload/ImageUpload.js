e1f5377fdc816e68014e1d8afca59381159074fa
firdausthedev <firdausthedev@gmail.com>
2021-12-20 07:31:52 +0800

    predict image

--- src/components/upload/ImageUpload.js
+++ src/components/upload/ImageUpload.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 function ImageUpload() {
   const [model, setModel] = useState("knife");
   const [file, setFile] = useState(null);
+  const [loadPred, isLoadPred] = useState(false);
+  const [showPred, setShowPred] = useState(false);
+  const [pred, setPred] = useState(0.0);
 
   const hiddenFileInput = React.useRef(null);
 
@@ -23,7 +26,49 @@ function ImageUpload() {
     }
   };
 
+  // When user click clear btn
   const onClearClick = (e) => {
+    setFile(null);
+  };
+
+  // When user click upload btn
+  const onUploadClick = () => {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("filename", file.name);
+    isLoadPred(true);
+    fetch("https://firdausthedev-webai.herokuapp.com/uploadimage", {
+      method: "POST",
+      body: formData,
+    }).then((response) => {
+      response.json().then((body) => {
+        const res = body.filename;
+        fetch(
+          "https://firdausthedev-webai.herokuapp.com/predictimage/" +
+            res +
+            "/" +
+            model +
+            ".pkl",
+          {
+            method: "GET",
+          }
+        ).then((response) => {
+          response.json().then((body) => {
+            const res = body.prob.model;
+            isLoadPred(false);
+            setShowPred(true);
+            setPred(res);
+            console.log(res);
+          });
+        });
+      });
+    });
+  };
+
+  // When user click close btn
+  const onClosePred = () => {
+    setPred(0.0);
+    setShowPred(false);
     setFile(null);
   };
 
@@ -69,16 +114,29 @@ function ImageUpload() {
             {file == null ? (
               <div className='upload-info'>
                 <p>UPLOAD IMAGE HERE</p>
-                <p>Accepting png, jpg and jpeg files.</p>
+                <p>Accepting image files such as png, jpg and jpeg.</p>
               </div>
-            ) : (
+            ) : loadPred == false && showPred == false ? (
               <div className='upload-image'>
                 <img src={URL.createObjectURL(file)} />
                 <p></p>
                 <div className='upload-image-btns'>
                   <a onClick={onClearClick}>Clear</a>
-                  <a>Upload</a>
+                  <a onClick={onUploadClick}>Upload</a>
                 </div>
+              </div>
+            ) : loadPred ? (
+              <div className='upload-loading'>
+                {/* <img src={URL.createObjectURL(file)} /> */}
+                <p>Loading...</p>
+              </div>
+            ) : (
+              <div className='upload-predict'>
+                <p>
+                  Prediction: {(pred * 100).toFixed(2)}% {model}
+                </p>
+                <img src={URL.createObjectURL(file)} />
+                <a onClick={onClosePred}>Close</a>
               </div>
             )}
           </div>
@@ -202,6 +260,23 @@ const Upload = styled.div`
       a:last-child {
         background: #77d970;
       }
+    }
+  }
+  .upload-predict {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    p {
+      font-size: 1rem;
+    }
+    img {
+      width: 150px;
+      border-radius: 10px;
+    }
+    a {
+      align-self: center;
+      margin-top: 15px;
     }
   }
 `;
