3c5e7e2bd4effc66bb51aba6293d94beeaa7e375
firdausthedev <firdausthedev@gmail.com>
2021-12-19 23:11:21 +0800

    Updates

---
+++ precache-manifest.bc9892fb3eb2c7fa5436c0f9ed7aa622.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a28069d243858ae1c332",
+    "url": "/webai/static/css/main.640becdc.chunk.css"
+  },
+  {
+    "revision": "a28069d243858ae1c332",
+    "url": "/webai/static/js/main.957b9a78.chunk.js"
+  },
+  {
+    "revision": "7c1d02499fccb3591a47",
+    "url": "/webai/static/js/runtime~main.b270b7e5.js"
+  },
+  {
+    "revision": "f691dfb0221b8c0c5fe8",
+    "url": "/webai/static/js/2.e5288514.chunk.js"
+  },
+  {
+    "revision": "5964539c0c791520a88e71736ee5613d",
+    "url": "/webai/index.html"
+  }
+];
