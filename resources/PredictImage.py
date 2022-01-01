06d5eccf71d35fd6152c401239cf02128fb91f29
firdausthedev <firdausthedev@gmail.com>
2022-01-01 09:19:49 +0800

    update models
    update model

--- resources/PredictImage.py
+++ resources/PredictImage.py
@@ -4,36 +4,38 @@ from PIL import Image
 import numpy as np
 from numpy import asarray
 
+
 def stride(img):
-      height, weight = img.shape
-    
-      for i in range(height - 2):
+    height, weight = img.shape
+
+    for i in range(height - 2):
         for j in range(weight - 2):
-          # 3x3 filter
-          region = img[i:(i + 3), j:(j + 3)]
-          yield region, i, j
-    
+            # 3x3 filter
+            region = img[i:(i + 3), j:(j + 3)]
+            yield region, i, j
+
+
 def f_prop(input, filters):
 
+    height, weight = input.shape
+    output = np.zeros((height - 2, weight - 2, 11))
 
-      height, weight = input.shape
-      output = np.zeros((height - 2, weight - 2, 8))
-
-      for region, i, j in stride(input):
+    for region, i, j in stride(input):
         output[i, j] = np.sum(region * filters, axis=(1, 2))
 
-      return output
+    return output
+
 
 def max_f(input):
-
 
     height, width, num_filters = input.shape
     output = np.zeros((height // 2, width // 2, num_filters))
 
     for region, i, j in max_stride(input):
-      output[i, j] = np.amax(region, axis=(0, 1))
+        output[i, j] = np.amax(region, axis=(0, 1))
 
     return output
+
 
 def max_stride(img):
 
@@ -42,15 +44,14 @@ def max_stride(img):
     new_width = width // 2
 
     for i in range(new_height):
-      for j in range(new_width):
-        region = img[(i * 2):(i * 2 + 2), (j * 2):(j * 2 + 2)]
-        yield region, i, j
+        for j in range(new_width):
+            region = img[(i * 2):(i * 2 + 2), (j * 2):(j * 2 + 2)]
+            yield region, i, j
+
 
 def soft_f(input, weights, biases):
- 
 
     input = input.flatten()
-
 
     totals = np.dot(input, weights) + biases
     total_last = totals
@@ -66,17 +67,16 @@ class PredictImage(Resource):
         weights = load_weight[1]
         biases = load_weight[2]
         t_img = Image.open("upload/"+name).convert('L')
-        t_img = t_img.resize((28, 28))
+        t_img = t_img.resize((64, 64))
         t_img = asarray(t_img)
-        
+
         out = f_prop((t_img / 255) - 0.5, filters)
         out = max_f(out)
-        
-        out = soft_f(out,weights, biases)
+
+        out = soft_f(out, weights, biases)
         # print(out)
-        
+
         return {"success": True, "prob": {
-            "model" : out[0],
-            "model2" : out[1]
-            }}, 200 
-        
+            "model": out[0],
+            "model2": out[1]
+        }}, 200
--- resources/TrainModel.py
+++ resources/TrainModel.py
@@ -12,7 +12,9 @@ from .MaxPooling import MaxPooling
 from .SoftMax import Softmax
 
 # extract and delete the zip files
-def extractZip (model1, model2):
+
+
+def extractZip(model1, model2):
     zf = ZipFile("dataset/"+model1, "r")
     zf.extractall('dataset')
     zf = ZipFile("dataset/"+model2, "r")
@@ -21,96 +23,88 @@ def extractZip (model1, model2):
     os.remove("dataset/"+model1)
     os.remove("dataset/"+model2)
 
-# split into train and test folder    
-def split_folder(dataset, indexFolder, x, y, xt, yt):
-    source_dir = 'dataset/' + dataset
-    target_train_dir = "dataset/" + dataset +"/train_set"
-    target_test_dir = "dataset/" + dataset +"/test_set"
+# split into train and test folder
 
-    # move files to train set folder
-    file_names = os.listdir(source_dir)
+
+def split_folder(model, indexFolder, x, y, xt, yt):
+
+    # remove the model extension and get the name
+    folder = os.path.splitext(model)[0]
+    os.makedirs("dataset/" + folder + "/train_set")
+    os.makedirs("dataset/" + folder + "/test_set")
+
+    source_dir = 'dataset/' + folder
+    target_train_dir = "dataset/" + folder + "/train_set"
+    target_test_dir = "dataset/" + folder + "/test_set"
+
+    file_names = os.listdir('dataset/' + folder)
     file_names.remove("train_set")
     file_names.remove("test_set")
 
-    # renaming file names
+    # rename each files in folder
     for index, file in enumerate(file_names):
-        os.rename(os.path.join(source_dir, file), os.path.join(source_dir, dataset.join([str(index), os.path.splitext(file)[1]])))
+        os.rename(os.path.join(source_dir, file), os.path.join(
+            source_dir, folder.join([str(index), os.path.splitext(file)[1]])))
 
     file_names = os.listdir(source_dir)
     file_names.remove("train_set")
     file_names.remove("test_set")
-
-    # file_names = sorted(file_names,key=lambda x: int(os.path.splitext(x)[0]))
-
 
     for index, file_name in enumerate(file_names):
-        if index < 800 :
-          shutil.move(os.path.join(source_dir, file_name), target_train_dir)
-          img = Image.open(target_train_dir +"/" + file_name).convert('L')
-          img = img.resize((28,28))
-          x.append(asarray(img))
-          if (indexFolder == 0):
-            y.append(0)
-          else:
-            y.append(1)
-        else :
-          shutil.move(os.path.join(source_dir, file_name), target_test_dir)
-          img = Image.open(target_test_dir +"/" + file_name).convert('L')
-          img = img.resize((28,28))
-          xt.append(asarray(img))
-          if (indexFolder == 0):
-            yt.append(0)
-          else:
-            yt.append(1)
+        if index < 800:
+            # move the file into train set folder
+            shutil.move(os.path.join(source_dir, file_name), target_train_dir)
+            # convert the image into grayscale
+            img = Image.open(target_train_dir + "/" + file_name).convert('L')
+            # resize the image
+            img = img.resize((64, 64))
+            # convert img into numpy array and add to x_train list
+            x.append(asarray(img))
 
-
-
+            if (indexFolder == 0):
+                y.append(0)
+            else:
+                y.append(1)
+        else:
+            shutil.move(os.path.join(source_dir, file_name), target_test_dir)
+            img = Image.open(target_test_dir + "/" + file_name).convert('L')
+            img = img.resize((64, 64))
+            xt.append(asarray(img))
+            if (indexFolder == 0):
+                yt.append(0)
+            else:
+                yt.append(1)
 
 
 class TrainModel(Resource):
     def get(self, model1, model2):
-        
+
+        # extract files
+        try:
+            extractZip(model1, model2)
+        except:
+            return {"success": False, "message": "Something went wrong"}, 406
+
         x_train = []
         y_train = []
         x_test = []
         y_test = []
+        acc_history = []
+        loss_history = []
 
-        # extract files
-        try:
-            extractZip(model1, model2) 
-        except:
-            return {"success": False, "message": "Something went wrong"}, 406
-        
-                
-        
-        
-        # get list of names
-        folder_list = os.listdir("dataset/")
-        
-        for folder in folder_list:
-            os.makedirs("dataset/" + folder +"/train_set")
-            os.makedirs("dataset/" + folder +"/test_set")
-        
-        try:
-            for index, folder in enumerate(folder_list):
-                split_folder(folder, index, x_train, y_train, x_test, y_test)
-        except :
-            return {"success": False, "message": "Something went wrong"}, 406
-        # split folder into training and test set
-      
-     
-        
+        split_folder(model1, 0,  x_train, y_train, x_test, y_test)
+        split_folder(model2, 1,  x_train, y_train, x_test, y_test)
+
         # convert labels into numpy array
         nx_train = np.asarray(x_train)
         ny_train = np.asarray(y_train)
         nx_test = np.asarray(x_test)
         ny_test = np.asarray(y_test)
-        
 
-        conv = Convolution(8)                  
-        pool = MaxPooling()                  
-        softmax = Softmax(13 * 13 * 8, 2) 
-        
+        conv = Convolution(11)
+        pool = MaxPooling()
+        softmax = Softmax(31 * 31 * 11, 2)
+
         def forward(image, label):
             output = conv.f_prop((image / 255) - 0.5)
             output = pool.f_prop(output)
@@ -120,11 +114,11 @@ class TrainModel(Resource):
             accuracy = 1 if np.argmax(output) == label else 0
 
             return output, loss, accuracy
-        
+
         def train(im, label, lr=.005):
             output, loss, acc = forward(im, label)
 
-            gradient = np.zeros(10)
+            gradient = np.zeros(2)
             gradient[label] = -1 / output[label]
 
             gradient = softmax.b_prop(gradient, lr)
@@ -132,41 +126,72 @@ class TrainModel(Resource):
             gradient = conv.b_prop(gradient, lr)
 
             return loss, acc
-        
-      
-        # cnn.accurate_test(nx_test, ny_test)
-        for epoch in range(1):
-          print('--- Epoch %d ---' % (epoch + 1))
-          
-          #shuffle the train and test list
-          permutation = np.random.permutation(len(nx_train))
-          nx_train = nx_train[permutation]
-          ny_train = ny_train[permutation]
-          
-          
-          loss = 0
-          num_correct = 0
-          for i, (im, label) in enumerate(zip(nx_train, ny_train)):
-            if i % 100 == 99:
-                  print('Step %d Average Loss %.3f | Accuracy: %d%%' %(i + 1, loss / 100, num_correct))
-                  loss = 0
-                  num_correct = 0
-                  break
-                  
-            l, acc = train(im, label)
-            loss += l
-            num_correct += acc
-        
-        saved_list = [conv.get_filters(), softmax.get_weights(), softmax.get_biases()]
-        pickle.dump( saved_list , open( 'trained_model/trained.pkl' , 'wb' ) )
 
-        print(saved_list)
-        # delete dataset after finish training
-        # for index, folder in enumerate(folder_list):
-        #     shutil.rmtree("dataset/" + folder)
+        # train model
+        isLooping = True
+
+        for epoch in range(1):
+            print('--- Epoch %d ---' % (epoch + 1))
+
+            permutation = np.random.permutation(len(nx_train))
+            nx_train = nx_train[permutation]
+            ny_train = ny_train[permutation]
+
+            loss = 0
+            num_correct = 0
+
+            for i, (im, label) in enumerate(zip(nx_train, ny_train)):
+
+                if not isLooping:
+                    break
+
+                if i % 100 == 99:
+                    print(
+                        'Step %d Average Loss %.3f | Accuracy: %d%%' %
+                        (i + 1, loss / 100, num_correct)
+                    )
+                    acc_history.append(num_correct)
+                    loss_history.append(loss/100)
+
+                    # early stopping
+                    if 0.05 <= loss / 100 <= 0.10 and num_correct >= 90:
+                        isLooping = False
+                        break
+
+                    loss = 0
+                    num_correct = 0
+                    break
+
+                l, acc = train(im, label)
+                loss += l
+                num_correct += acc
+
+        # accuracy test
+        def predict_test(limit):
+            acc = 0
+
+            for index, img in enumerate(nx_test[:limit]):
+                out = conv.f_prop((img / 255) - 0.5)
+                out = pool.f_prop(out)
+                out = softmax.f_prop(out)
+
+                exp = 0
+                if(out[0] > out[1]):
+                    exp = 0
+                else:
+                    exp = 1
+
+                a_output = 0 if ny_test[index] == 0 else 1
+
+                if(a_output == exp):
+                    acc += 1
+                print("Accuracy: ", (acc/limit)*100)
+
+            return (acc/limit)*100
+
+        saved_list = [conv.get_filters(), softmax.get_weights(
+        ), softmax.get_biases(), predict_test(20), acc_history, loss_history]
+        pickle.dump(saved_list, open('trained_model/trained.pkl', 'wb'))
+
         shutil.rmtree("dataset")
-        print(len(nx_train))
         return {"success": True, "filename": "trained"}, 200
-      
-        
-        
